0c40ee36c67adf25e57f6dfc28456b5b21005d54
joschif <jonas.simon.fleck@gmail.com>
2022-05-09 19:55:54 +0200

    `NetworkGraph` getter

--- R/generics.R
+++ R/generics.R
@@ -40,6 +40,12 @@ get_network_graph <- function(object, ...){
     UseMethod(generic = 'get_network_graph', object = object)
 }
 
+#' @rdname NetworkGraph
+#' @export NetworkGraph
+NetworkGraph <- function(object, ...){
+    UseMethod(generic = 'NetworkGraph', object = object)
+}
+
 #' @rdname GetGRN
 #' @export GetGRN
 GetGRN <- function(object, ...){
--- tests/test_manual.R
+++ tests/test_manual.R
@@ -49,6 +49,8 @@ test_srt <- infer_grn(test_srt, genes=genes_use,
 test_srt <- find_modules(test_srt, min_genes_per_module=0, nvar_thresh=2)
 test_srt <- get_network_graph(test_srt, n_neighbors=2)
 
+NetworkGraph(test_srt)
+
 plot_gof(test_srt, point_size=2)
 plot_module_metrics(test_srt)
 
